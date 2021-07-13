155302b1b9533b9493a6a9e73389f409f4c98c77
MK <m.kiffel@gmail.com>
2021-07-13 12:23:35 +0100

    smileFrame implicit method

--- Decisions/src/Models.scala
+++ Decisions/src/Models.scala
@@ -7,6 +7,7 @@ import smile.io.Read
 import org.apache.commons.csv.CSVFormat 
 
 import decisions.CompareSystems.utils._
+import decisions.TransactionsData._
 
 object Dataset{
     trait DecisionDataset
@@ -24,6 +25,7 @@ object Systems{
     case class RF(name: String) extends Recognizer
     case class Isotonic(name: String) extends Calibrator
     case class Platt(name: String) extends Calibrator
+    case class Bionomial(name: String) extends Calibrator
     case object Uncalibrated extends Calibrator
 
     type System = Tuple2[Recognizer, Calibrator]    
@@ -50,7 +52,30 @@ class SmileKitLearn[T](model: T){
             }
         }
     }
-}             
+    def predictproba(data: Array[Double]): Array[Double] = model match {
+        case m: IsotonicRegressionScaling => {
+            data.map(m.predict)
+        }
+    }
+    def predictProba(data: Array[Double]): Double = model match {
+        case m:LogisticRegression => {
+            val tempScore = new Array[Double](2) // 2 classes
+            m.predict(data, tempScore)
+            tempScore(1)
+        }
+        case m:RandomForest => {
+            val schema = m.schema // model schema excludes the target variable...
+            val dataTupled = Tuple.of(data, schema)
+            val tempScore = new Array[Double](2) // 2 classes
+            m.predict(dataTupled, tempScore)
+            tempScore(1)
+        }
+    }
+    def predictProba(data: Double): Double = model match {
+        case m: IsotonicRegressionScaling => m.predict(data)
+    }
+}
+
 
 object SmileKitLearn {
     implicit def logitToLogit(m: LogisticRegression): SmileKitLearn[LogisticRegression] =
@@ -61,24 +86,40 @@ object SmileKitLearn {
 
 
 
-class SmileFrame{
-    val format = CSVFormat.DEFAULT.withFirstRecordAsHeader()
+class SmileFrame(data: Array[Transaction]){
+    /* Converts an Array[Array[AnyVal]] to a Smile DataFrame. 
+    
+    Achieved by saving the array to CSV and loading it back. That's not great, but it's all I have
+    because doing in-memory conversion throws unreadable errors. 
+    
+    I can convert the array to a Smile Tuple and apply ofDataFrame() but it throws an error
+    when the Tuples are of mixed primitive types, which is a requirement here.
+    */
+    def asDataFrame(schema: StructType, rootPath: String): DataFrame = {
+        val format = CSVFormat.DEFAULT.withFirstRecordAsHeader()
 
-    def asDataFrame(data: Seq[String], schema: StructType, dataType: Dataset.DecisionDataset, rootPath: String): DataFrame = dataType match {
-        case t:Dataset.RecognizerDataset => {
-            writeFile(s"$rootPath/train.csv", data, "label,amount,count")
-            val train: DataFrame = Read.csv(s"$rootPath/train.csv", format, schema)
-            train
+        def caseclassToString(tr: Transaction) = tr match {
+            case Transaction(la, am, cnt) => f"$la, $am%1.2f, $cnt%1.2f"
+            case _ => "not a transaction."
         }
-        case t:Dataset.CalibrationDataset => {
-            writeFile(s"$rootPath/calibration.csv", data, "label,score")
-            val calibration: DataFrame = Read.csv(s"$rootPath/calibration.csv", format, schema)
-            calibration
-        }
+
+        //if (schema.fields.size != data.size)
+        //throw new Exception("Number of fields don't match. Check the schema.")
+
+        val stringified: Array[String] = for {
+            row <- data
+        } yield caseclassToString(row)
+        
+        val fields = schema.fields.map(_.name).mkString(",")
+
+        writeFile(s"$rootPath/temp.csv", stringified, fields)
+
+        val df: DataFrame = Read.csv(s"$rootPath/temp.csv", format, schema)
+        df
     }
 }
 
 object SmileFrame {
-    implicit def ArrayToDataFrame(d: Array[Array[String]]): SmileFrame =
-        new SmileFrame()
+    implicit def ArrayToDataFrame(d: Array[Transaction]): SmileFrame =
+        new SmileFrame(d)
 }
